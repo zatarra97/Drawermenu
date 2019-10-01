82cea52bf019f4978629fc971ab8fe1d18dd4844
Emmanuele Carlone <emmanuele.carlone@gmail.com>
2019-10-01 16:11:41 +0200

    Primo commit, creazione drawer laterale da zero

---
+++ app/src/main/java/com/example/drawer_menu/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.drawer_menu;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Per aggiungere l'icona di apertura e chiusura della barra laterale senza dover trascinare il touch
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+
+    //Per restituire il valore corretto se chiudere o aprire il menu laterale
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
